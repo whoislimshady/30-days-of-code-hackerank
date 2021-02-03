66fa6beaab445fac93c79217c172f358eb5b1920
Harsh <72891217+whoislimshady@users.noreply.github.com>
2021-02-03 13:08:38 +0530

    Add files via upload

---
+++ intergertobinary.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+string BinaryString(int number)
+{ 
+    //base cases
+    if(number == 0)
+    {
+     return "0";
+    }
+    if (number == 1)
+    {
+        return "1";//base two
+
+    }else
+    {
+        return BinaryString(number /2 ) + BinaryString(number % 2);
+    }
+    
+    
+}
+int main(){
+    //string bin;
+   // bin = BinaryString(1);
+    //cout<<bin;
+   // cout<<BinaryString(0)<<endl;
+      cout<<BinaryString(17) << endl;    
+    return 0;
+} 
